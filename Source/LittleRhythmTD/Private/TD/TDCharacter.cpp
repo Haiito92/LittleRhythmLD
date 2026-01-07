26de3212db9010018774b649da419b53b43aaf31
Haiito92 <hannaantoinebusiness@gmail.com>
2026-01-07 17:28:33 +0100

    Start of Player

---
+++ Source/LittleRhythmTD/Private/TD/TDCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Public/TD/TDCharacter.h"
+
+
+// Sets default values
+ATDCharacter::ATDCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ATDCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATDCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ATDCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
---
+++ Source/LittleRhythmTD/Public/TD/TDPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TDPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LITTLERHYTHMTD_API ATDPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
